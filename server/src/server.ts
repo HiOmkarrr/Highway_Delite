08dcc4b922de19f1215b04407c60941aea5ddc94
HiOmkarrr <omkarmondkar6@gmail.com>
2025-10-30 14:46:41 +0530

    Add production env and improve error handling

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,3 +1,16 @@
+/**
+ * Highway Delite - Travel Experiences Booking Platform
+ * Copyright (c) 2025 Omkar (HiOmkarrr)
+ * 
+ * This software was developed as part of an internship application.
+ * All rights reserved. Unauthorized use, reproduction, or distribution 
+ * of this code without express written permission is strictly prohibited.
+ * 
+ * Author: Omkar
+ * GitHub: @HiOmkarrr
+ * Created: October 2025
+ */
+
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
